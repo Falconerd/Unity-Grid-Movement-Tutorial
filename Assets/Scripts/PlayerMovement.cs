8f55ed726c1e9ceb0ee0e883034066add5f16fad
Dylan Falconer <me@falconerd.com>
2020-03-19 01:42:20 +1100

    Episode 1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	float moveSpeed = 0.25f;
+	[SerializeField]
+	float rayLength = 1.4f;
+	[SerializeField]
+	float rayOffsetX = 0.5f;
+	[SerializeField]
+	float rayOffsetY = 0.5f;
+	[SerializeField]
+	float rayOffsetZ = 0.5f;
+
+	Vector3 targetPosition;
+	Vector3 startPosition;
+	bool moving;
+
+	void Update() {
+		if (moving) {
+			if (Vector3.Distance(startPosition, transform.position) > 1f) {
+				transform.position = targetPosition;
+				moving = false;
+				return;
+			}
+
+			transform.position += (targetPosition - startPosition) * moveSpeed * Time.deltaTime;
+			return;
+		}
+
+		Debug.DrawLine(transform.position + Vector3.up * rayOffsetY + Vector3.right * rayOffsetX, transform.position + Vector3.up * rayOffsetY + Vector3.right * rayOffsetX + Vector3.forward * rayLength, Color.red, Time.deltaTime);
+		Debug.DrawLine(transform.position + Vector3.up * rayOffsetY - Vector3.right * rayOffsetX, transform.position + Vector3.up * rayOffsetY - Vector3.right * rayOffsetX + Vector3.forward * rayLength, Color.red, Time.deltaTime);
+
+		Debug.DrawLine(transform.position + Vector3.up * rayOffsetY + Vector3.right * rayOffsetX, transform.position + Vector3.up * rayOffsetY + Vector3.right * rayOffsetX + Vector3.back * rayLength, Color.red, Time.deltaTime);
+		Debug.DrawLine(transform.position + Vector3.up * rayOffsetY - Vector3.right * rayOffsetX, transform.position + Vector3.up * rayOffsetY - Vector3.right * rayOffsetX + Vector3.back * rayLength, Color.red, Time.deltaTime);
+
+		Debug.DrawLine(transform.position + Vector3.up * rayOffsetY + Vector3.forward * rayOffsetZ, transform.position + Vector3.up * rayOffsetY + Vector3.forward * rayOffsetZ + Vector3.left * rayLength, Color.red, Time.deltaTime);
+		Debug.DrawLine(transform.position + Vector3.up * rayOffsetY - Vector3.forward * rayOffsetZ, transform.position + Vector3.up * rayOffsetY - Vector3.forward * rayOffsetZ + Vector3.left * rayLength, Color.red, Time.deltaTime);
+
+		Debug.DrawLine(transform.position + Vector3.up * rayOffsetY + Vector3.forward * rayOffsetZ, transform.position + Vector3.up * rayOffsetY + Vector3.forward * rayOffsetZ + Vector3.right * rayLength, Color.red, Time.deltaTime);
+		Debug.DrawLine(transform.position + Vector3.up * rayOffsetY - Vector3.forward * rayOffsetZ, transform.position + Vector3.up * rayOffsetY - Vector3.forward * rayOffsetZ + Vector3.right * rayLength, Color.red, Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+			if (CanMove(Vector3.forward)) {
+				targetPosition = transform.position + Vector3.forward;
+				startPosition = transform.position;
+				moving = true;
+			}
+		} else if (Input.GetKeyDown(KeyCode.S)) {
+			if (CanMove(Vector3.back)) {
+				targetPosition = transform.position + Vector3.back;
+				startPosition = transform.position;
+				moving = true;
+			}
+		} else if (Input.GetKeyDown(KeyCode.A)) {
+			if (CanMove(Vector3.left)) {
+				targetPosition = transform.position + Vector3.left;
+				startPosition = transform.position;
+				moving = true;
+			}
+		} else if (Input.GetKeyDown(KeyCode.D)) {
+			if (CanMove(Vector3.right)) {
+				targetPosition = transform.position + Vector3.right;
+				startPosition = transform.position;
+				moving = true;
+			}
+		}
+	}
+
+	bool CanMove(Vector3 direction) {
+		if (Vector3.Equals(Vector3.forward, direction) || Vector3.Equals(Vector3.back, direction)) {
+			if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY + Vector3.right * rayOffsetX, direction, rayLength)) return false;
+			if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY - Vector3.right * rayOffsetX, direction, rayLength)) return false;
+		}
+		else if (Vector3.Equals(Vector3.left, direction) || Vector3.Equals(Vector3.right, direction)) {
+			if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY + Vector3.forward * rayOffsetZ, direction, rayLength)) return false;
+			if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY - Vector3.forward * rayOffsetZ, direction, rayLength)) return false;
+		}
+		return true;
+	}
+}
